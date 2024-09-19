51cf4b15713a69b7f5812c5ca3ee6038e9cccff4
Ashfan Dafa <ashfandafa12@gmail.com>
2024-09-19 09:29:59 +0700

    Add files via upload

---
+++ hello2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<"=======\n";
+	cout<<"hello world\n";
+	cout<<"=======\n";
+	cout<<"Nama\t: Ashfan Dafa Hibrizi\n";
+	cout<<"NIM\t: 24.11.5985\n";
+	cout<<"Jurusan\t: Informatika\n";
+	cout<<"Asal\t: Klaten\n";
+	
+}
